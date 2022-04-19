9d20cde28af1b8d7a9b9060a466ceea44db48761
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-04-19 16:23:16 -0400

    Create firstprogram.cpp

---
+++ basic c++ programs/firstprogram.cpp
@@ -0,0 +1,8 @@
+// Your First C++ Program
+
+#include <iostream>//header file for cout
+using namespace std;//go to namespace and use the cout present in that directory
+int main() {
+    std::cout << "Hello World!" << endl;
+    return 0;
+}
