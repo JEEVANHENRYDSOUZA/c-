65763d1a4fd8eca3dab4151a3485827a9104a8d9
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-04-19 17:01:37 -0400

    Create relational,boolean,logical,biwtiseoperator.cpp

---
+++ basic c++ programs/relational,boolean,logical,biwtiseoperator.cpp
@@ -0,0 +1,15 @@
+// Your First C++ Program
+
+#include <iostream>//header file for cout
+using namespace std;//go to namespace and use the cout present in that directory
+int main() {
+   
+   //relational operarotor
+   // <,> .....!=,==
+   // relation operator ouput will always be true or false
+    std::cout << "Hello World!" << endl;//endl for new line
+    //arithetic operators +,-,/,%
+    //logical operator &&,||,!----check the truth table for these operators --https://en.wikipedia.org/wiki/Truth_table
+    //bitwise operator works on bit level --- &,!,|
+    return 0;
+}
