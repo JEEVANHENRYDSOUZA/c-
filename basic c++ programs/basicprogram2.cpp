df7cf45c0afd43954d0e2107efe85e12583272d2
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-04-19 16:33:28 -0400

    Create basicprogram2.cpp

---
+++ basic c++ programs/basicprogram2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>//header file for cout
+using namespace std;//go to namespace and use the cout present in that directory
+int main() {
+    int a=5;//c++ is not dynamicallty typed language hence we have to manually do this
+            //here int is the data type  
+            //a is the variable that has the address to that memory allocated to it
+            //it also takes the size of the data how much memory needs to be allocated to it
+    char sample='k';
+    bool sample_2=true;
+    float sample_3=10.3;
+    double sample_4=11.12231;
+    //convetion to name variable do not start the name with numbers
+    //we can also use _ at the start
+
+    std::cout << "Hello World!" << endl;//endl for new line
+    return 0;
+}
