faee435bf7be2e74311e21695e2ffe5da8e87d3a
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-04-20 15:25:21 -0400

    Create whileloop.cpp

---
+++ basic c++ programs/whileloop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+   cin >> a;
+   while(a){
+       cout << "when conditon is true" <<endl; //make sure that the loop is not infinite loop
+       //if it is inifinite loop make sure that breaks at some condition 
+   }
+    
+    
+    return -1;
+}
