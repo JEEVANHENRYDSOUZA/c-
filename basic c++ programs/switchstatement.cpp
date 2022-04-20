29a5c5c10b2adf9f30bdb8a1f933a845cd071a12
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-04-20 15:15:32 -0400

    Create switchstatement.cpp

---
+++ basic c++ programs/switchstatement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin >> a;
+    switch(a){
+        case 1: cout <<"number is one"<<endl;
+                //break;//if break not written all statements written below it are executed irrespective of the value of a
+                
+        default: cout <<"this is default case";
+                    
+        
+        
+    }
+    
+    
+    return -1;
+}
