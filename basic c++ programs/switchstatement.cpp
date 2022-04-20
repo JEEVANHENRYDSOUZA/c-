5741b70713ae0e95c6d683b254b88ea55995f7ab
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-04-20 15:20:33 -0400

    Update switchstatement.cpp

--- basic c++ programs/switchstatement.cpp
+++ basic c++ programs/switchstatement.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main(){
     int a;
-    cin >> a;
+    cin >> a;//remeber a should evaluate to a value always to use it in switch case
+
     switch(a){
         case 1: cout <<"number is one"<<endl;
                 //break;//if break not written all statements written below it are executed irrespective of the value of a
