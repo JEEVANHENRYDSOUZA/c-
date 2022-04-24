c106b06d62489e140144dc8c98b8a043a81fc9c2
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-04-24 13:24:47 -0400

    Create reversing a number.cpp

---
+++ basic c++ programs/reversing a number.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+    int reverse(int x) {
+        int ans = 0;
+        int check;
+        if(x<0){
+            check=x;
+            x=x*(-1);
+        }
+        while(x != 0){
+            int digit = x%10;
+            if(ans >= INT_MIN/10 && ans <= INT_MAX/10){
+            ans = ans*10 + digit;
+            }
+            else{
+                break;
+            }
+            x = x/10;
+            
+        }
+        if(x == 0) 
+        {   if (check < 0){
+            ans=ans*-1;
+        }
+            
+            return ans;
+        
+        }
+        else return false;
+
+    }
+
+
+int main(){
+
+    int returned_value;
+    int number;
+    cout <<"enter the number that you want to reverse" << endl;
+    cin >> number;
+    returned_value=reverse(number);
+    cout << returned_value << endl;
+    return 0;
+}
