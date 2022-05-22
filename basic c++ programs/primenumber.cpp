cbfdeab9c0e71c0da4e61bd8d2ea13846092f7f5
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-05-21 20:45:36 -0400

    Create primenumber.cpp

---
+++ basic c++ programs/primenumber.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void prime(int n){
+    int count =0;
+    for(int i=1;i<=n;i++){
+        if(i%2==0){
+            count++;
+        }
+    }
+
+    if(count>2){
+        cout <<"not a prime number"<<endl;
+    }
+    else{
+        cout<<"it is a prime number"<<endl;
+    }
+
+
+}
+int main(){
+    cout<<"basic maths for dsa"<<endl;
+
+    cout<<"prime number"<<endl;
+    int n;
+    cin>>n;
+    prime(n);
+    return 0;
+}
