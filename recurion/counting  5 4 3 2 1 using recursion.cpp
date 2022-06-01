88408d3b66f9d54546a6e15bb6416081c3169ec8
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-06-01 11:21:59 -0400

    Create counting  5 4 3 2 1 using recursion.cpp

---
+++ recurion/counting  5 4 3 2 1 using recursion.cpp
@@ -0,0 +1,24 @@
+{
+    cout<<"recursion sucks"<<endl;
+    //recursion is when function calls itself
+    //this is recursion
+    //breaking down bigger problems into similar type of smaller problems then use recursion
+    //example 5! can also be written as 5*4!
+    //another example is 2^10 can be written as 2*2^9
+    //so try for recursion when we can break down larger problems smaller problems of similar type
+    //things which are mandatory in recursion
+    //1.base case
+    //2.recursive call
+    //what is base case?----->implies where to stop or terminating condition as well
+    //types of recursion
+    //1.head recursion----->recursive call at the start
+    //2.tail recursion---->recursive call is made at the end
+    //not required to know just for info
+    //example 1. of recursion-->factorial printing
+    int n;
+    cin>>n; 
+    count(n);
+
+
+    return 0;
+}
