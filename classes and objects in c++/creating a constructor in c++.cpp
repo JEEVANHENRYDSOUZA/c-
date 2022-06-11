608c0f89c7ab41b5296a7ca5f3cd84ee674e1f9c
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-06-11 13:43:49 -0400

    Create creating a constructor in c++.cpp

---
+++ classes and objects in c++/creating a constructor in c++.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class human{
+    public:
+    human(){//same name as that of the class with paranthesis
+        int age=10;
+        cout<<"i am constructor "<<endl;//it should always be public and not private
+    }
+    char gender;
+    int age=23;
+    public:
+   
+    void setage(int a){
+        age=a;
+    }
+
+    int getage(){
+        return age;
+    }   
+
+    
+
+
+};
+
+
+int main(){
+    cout<<"we will be learning object oriented programming in c++"<<endl;
+    //what is an object--->a real world object is called as ano object
+    //what is a class---->a class  is like a blueprint for the object
+    // a class is defined by two things:
+    //1.property or also called as attributes
+    //2.behaviour
+    //access specifier c++ as 3
+    //1.private---access within class default case----using getter and setter we can access it outside 
+    //2.protected
+    //3.public---access anywhere
+    //memory is assigned when we create the object
+    //when memory is assigned then we do padding
+    human *obj=new human();
+    obj->setage(90);
+    cout<<obj->getage()<<endl;
+
+    return 0;
+}
