368314cc942fbb2809f59e79aa8cabfdb2785a72
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-06-13 10:53:57 -0400

    Create encapsulation.cpp

---
+++ oops feature in c++/encapsulation.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+class human{
+    private:
+    int age;
+    char name;
+
+
+
+
+    public://encapsulation achieved
+    int getage(){
+        return this->age;
+    }
+
+
+
+};
+int main(){
+    //what is oops?
+    //it is a programming paradigm
+    //in this we use objects and classes o program a given solution
+    //object creation ----1.static
+    //2.dynamic
+    //3.call automatically manually use delete
+    //copy constructor
+    //normal constructor
+    //parametrized constructor
+    //access modifier 
+    //public
+    //private
+    //protected
+    //public------ within class,outside class
+    //private---within class , no for outside class
+    //protected----within class , outside using inheritance
+    //empty class--does not give any error
+    //empty class is size of 1------reason is it is the minimum space the compiler has
+    //four pillars of oops:
+    //1.inheritance
+    //2.polymorphism
+    //3.encapsulation
+    //4.
+
+
+
+    //1.encapsulation:binding data member and functions together is called encapsulation
+    //when a class has both private data members and methods then we have achieved high encapsulation
+    //advantages of this:clean code,reusability,increases security
+
+
+
+
+
+
+
+
+    human obj;
+    
+    cout<<"the oops part 2"<<endl;
+
+
+
+
+    return 0;
+}
