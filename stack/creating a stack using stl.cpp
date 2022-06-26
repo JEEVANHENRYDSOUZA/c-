80c29904ccf4a4745c73d4041664f39760710c92
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-06-25 21:45:42 -0400

    Create creating a stack using stl.cpp

---
+++ stack/creating a stack using stl.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+int main(){
+
+    stack<int> st;
+    st.push(5);
+    st.push(7);
+    st.push(9);
+    cout<<st.empty()<<endl;
+    cout<<st.top()<<endl;
+    cout<<st.top();
+    st.pop();
+    
+
+
+
+
+
+    return 0;
+
+
+
+}
